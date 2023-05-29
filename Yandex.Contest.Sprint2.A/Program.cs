5f2e43707da3625a8c8cb5fcd2f3719587fcfc2c
Energizet <a2437@yandex.ru>
2023-05-29 23:14:28 +0300

    Sprint2.A

--- Yandex.Contest.Sprint2.A/Program.cs
+++ Yandex.Contest.Sprint2.A/Program.cs
@@ -1,12 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
-namespace Yandex.Contest.Sprint2.A
+internal static class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		var n = Contest.GetValue(int.Parse);
+		var m = Contest.GetValue(int.Parse);
+
+		var matrix = new int[m][];
+		for (var i = 0; i < m; i++)
 		{
-			Console.WriteLine("Hello World!");
+			matrix[i] = new int[n];
 		}
+		
+		for (var i = 0; i < n; i++)
+		{
+			var line = Contest.GetArray(int.Parse);
+			for (var j = 0; j < m; j++)
+			{
+				matrix[j][i] = line[j];
+			}
+		}
+
+		foreach (var line in matrix)
+		{
+			line.Print();
+		}
+	}
+}
+
+
+/// <summary>
+/// I/O методы
+/// </summary>
+internal static class Contest
+{
+	public static string GetValue()
+	{
+		return Console.ReadLine()!;
+	}
+
+	public static T GetValue<T>(Func<string, T> func)
+	{
+		return func(GetValue());
+	}
+
+	public static IEnumerable<T> GetIEnumerable<T>(Func<string, T> func,
+		StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries)
+	{
+		return GetValue()
+			.Split(new[] {' ', '\t'}, splitOptions)
+			.Select(func);
+	}
+
+	public static List<T> GetList<T>(Func<string, T> func,
+		StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries)
+	{
+		return GetIEnumerable(func, splitOptions).ToList();
+	}
+
+	public static List<string> GetList(
+		StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries)
+	{
+		return GetList(str => str, splitOptions);
+	}
+
+	public static T[] GetArray<T>(Func<string, T> func,
+		StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries)
+	{
+		return GetIEnumerable(func, splitOptions).ToArray();
+	}
+
+	public static string[] GetArray(
+		StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries)
+	{
+		return GetArray(str => str, splitOptions);
+	}
+
+	public static string Join<T>(this IEnumerable<T> arr, string separator = " ")
+	{
+		return string.Join(separator, arr);
+	}
+
+	public static void Print<T>(this IEnumerable<T> arr, string separator = " ")
+	{
+		arr.Join(separator).Print();
+	}
+
+	public static void Print(this ValueType value)
+	{
+		Console.WriteLine(value);
+	}
+
+	public static void Print(this object obj)
+	{
+		Console.WriteLine(obj);
+	}
+
+	public static void Print(this string str)
+	{
+		Console.WriteLine(str);
 	}
 }
